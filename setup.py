43a62ae2ef3969268b5f8bb752c3b38eab75ccc6
hunglin <hunglin59638@gmail.com>
2021-08-03 23:58:51 +0800

    add setup.py to install ffmpeg and python packages

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 07:17:55 2021
+
+@author: hunglin
+"""
+import os
+import sys
+import subprocess
+from pathlib import Path
+
+root_dir = Path(__file__).parent
+# install python packages
+subprocess.call(["pip", "install", "-r", root_dir/"requirements.txt"])
+
+# install ffmpeg
+lib_dir = root_dir / "lib"
+os_dir = lib_dir / sys.platform
+os_dir.mkdir(exist_ok=True, parents=True)
+os.chdir(os_dir)
+
+if sys.platform == "linux":
+    link = "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz"
+    if not (os_dir/"ffmpeg/ffmpeg").is_file():
+        os.system(f"wget -qcO- {os_dir/Path(link).name} {link} | tar -Jxf -")
+
+elif sys.platform == "win32":
+    link = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip"
+    if not (os_dir/"ffmpeg/bin/ffmpeg").is_file():
+        os.system(f"wget -qc {link}")
+        os.system(f"unzip {Path(link).name} && rm {Path(link).name}")
+        
+for file in os_dir.iterdir():
+    if file.is_dir() and file.name.startswith("ffmpeg"):
+        file.rename(os_dir/"ffmpeg")
+print("Setup finished.")
